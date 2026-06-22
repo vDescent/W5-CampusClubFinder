fc902ce834c48d4143cf66a3c14958118815c0a6
vdescent <timotiusryjaska@gmail.com>
2026-06-22 11:42:45 +0700

    fix: fix button in home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router"
+
 export default function Home(){
+    const navigate = useNavigate();
+
+    const handleGoClub = () =>{
+        navigate('/clubs')
+    }
 
     return(
         <div className="relative w-full min-h-screen bg-cover overflow-hidden p-2" style={{backgroundImage: `url('/img/05_art.jpg')`}}>
@@ -8,7 +15,7 @@ export default function Home(){
             <div className="relative z-10 flex flex-col justify-center items-start h-screen px-20">
                 <h1 className="text-white text-6xl my-5">Join your <span className="text-amber-600">dream</span> club</h1>
                 <h2 className="text-white text-2xl">Join and Contribute with community</h2>
-                <button className="p-6 text-black hover:cursor-pointer bg-white my-2 rounded-3xl hover:text-white hover:bg-amber-600">Join Now</button>
+                <button onClick={handleGoClub} className="p-6 text-black hover:cursor-pointer bg-white my-2 rounded-3xl hover:text-white hover:bg-amber-600">Join Now</button>
             </div>
 
         </div>
