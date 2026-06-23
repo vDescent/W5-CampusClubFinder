35b60026b1d791e6c3ca577c1a875843a42ecffd
vdescent <timotiusryjaska@gmail.com>
2026-06-23 14:04:12 +0700

    fix: fix clublist not done yet

--- src/pages/ClubList.jsx
+++ src/pages/ClubList.jsx
@@ -11,40 +11,34 @@ export default function ClubList(){
 
 
     return(
-        <div className="h-screen">
-            <h1>Sortby</h1>
-            <select name="" id="">
-                <option value="">Name A-Z</option>
-                <option value="">Name Z-A</option>
-            </select>
-            <h1>Mode</h1>
-            <select name="" id="" value={viewMode} onChange={(e) => setViewMode(e.target.value)}>
-                <option value="grid">Grid</option>
-                <option value="list">List</option>
-            </select>
+        <div className="min-h-screen">
+            <div className="flex justify-center gap-2">
+                <h1>Sortby</h1>
+                <select name="" id="">
+                    <option value="">Name A-Z</option>
+                    <option value="">Name Z-A</option>
+                </select>
+                <h1>Mode</h1>
+                <select name="" id="" value={viewMode} onChange={(e) => setViewMode(e.target.value)}>
+                    <option value="grid">Grid</option>
+                    <option value="list">List</option>
+                </select>
+            </div>
 
             {/* Ketika GRID */}
 
-            <div className={viewMode === "grid" ? "grid grid-cols-3 gap-4 justify-center m-4" : "flex flex-col gap-2 m-4"}>
+            <div className={viewMode === "grid" ? "grid grid-cols-3 gap-4 justify-center m-4" : "flex flex-col gap-2 max-w-xl mx-auto"}>
                 {clubs.map((club) =>(
-                    <div key={club.id} className="border p-2">
+                    <div key={club.id} className="flex flex-col border p-6 gap-2">
                         <h2 className="text-2xl">{viewMode === "grid" ? "Grid" : "List"} {club.name}</h2>
                         <p>{club.shortDescription}</p>
-                        <img src={club.image} alt=""/>
+                        <img src={club.image} alt="Club photos" className=" h-full"/>
+                        <button className="bg-amber-500 rounded-xl p-3 text-white hover:bg-black hover:cursor-pointer">See more</button>
                         {/* <p>{}</p> */}
                     </div>
                 ))}
             </div>
 
-            {/* KETIKA  List
-            <div>
-                {clubs.map((club) =>(
-                    <div key={club.id} className="border p-2">
-                        <h2>LIST {club.name}</h2>
-                    </div>
-                ))}
-            </div> */}
-            
         </div>
     )
 }
