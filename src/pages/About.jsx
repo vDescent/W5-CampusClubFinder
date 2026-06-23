447dd0fe141149d433412a44a7b1da78afdc23d4
vdescent <timotiusryjaska@gmail.com>
2026-06-23 13:00:46 +0700

    chore: add google-sans font

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,13 @@
 export default function About(){
     return(
-        <div>
+        <div className="flex flex-col p-6 max-w-2xl mx-auto gap-2">
             <h1 className="text-5xl">About</h1>
             <h2 className="text-4xl">What this app does ?</h2>
             <p>This app is simulate how club finder website run via localstorage. In this app user can explore club that available to user, so user can join the club.</p>
-            <h2 className="text-lg">Tech stack used in this project</h2>
-            <h3 className="text-3xl">React, tailwind</h3>
+            <h2 className="text-3xl">What Tech stack used in this project ?</h2>
+            <h3 className="text-xl">React and TailwindCss</h3>
             <p>This project using react for the frontend interfaces, and tailwind for the styling.</p>
-            <p>Explain what the app does and tech stack used.</p>
+            {/* <p>Explain what the app does and tech stack used.</p> */}
         </div>
     )
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,8 +8,8 @@ export default function NotFound(){
     }
 
     return(
-        <div className="min-h-screen flex flex-col items-center justify-center">
-            <h1>Sorry</h1>
+        <div className="min-h-screen flex flex-col items-center justify-center pb-5">
+            <h1 className="text-2xl">Sorry</h1>
             <p>Page that you looking for is not found :(</p>
             <button onClick={handleGoHome} className="bg-amber-500 p-4 rounded-2xl my-2 hover:bg-black hover:text-white hover:cursor-pointer">Back to home</button>
         </div>
